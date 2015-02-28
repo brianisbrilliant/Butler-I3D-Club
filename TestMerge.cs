e3b0f619f3397f57e61989dff1579187691c721f
brianisbrilliant <brianisbrilliant@users.noreply.github.com>
2015-02-28 16:16:25 -0600

    create testMerge.cs
    i wanna know how it works to merge the same file into itself. it surely wouldn't work for an image file or an FBX, but it might work for a text document.

---
+++ TestMerge.cs
@@ -0,0 +1,19 @@
+// what if i have some code and two people are working on it at the same time.
+// BH is working on the void Start() method.
+// and BF is working on the void Update() method.
+
+using UnityEngine;
+Using System.Collections.Generic;
+
+public class TestMerge : MonoBehaviour
+{
+    void Start()
+    { 
+  
+    }
+  
+    void Update()
+    {
+  
+    }
+}
