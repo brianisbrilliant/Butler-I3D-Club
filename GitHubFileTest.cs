17ce52bc17843fd71e2419e90d205bf4e7a65186
Brian Foster <brianisbrilliant@gmail.com>
2015-02-28 15:39:21 -0600

    second update today
    this is my second update today, i wonder if it will show me the
    different changes i’ve made today.

--- GitHubFileTest.cs
+++ GitHubFileTest.cs
@@ -1,3 +1,14 @@
 // brian foster
 // i3d club feb 2015
 // this is a test of GitHub, i don't know what it can do.
+
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GitHubScript : MonoBehaviour
+{
+	void Start()
+	{
+		Debug.Log("So, I just work in the folder, and use GitHub to update the changes.");
+	}
+}
